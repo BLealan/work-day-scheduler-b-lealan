45fe63cee4f801f1097b491763400e75552de018
BLealan <143086275+BLealan@users.noreply.github.com>
2023-11-02 19:14:16 +0000

    get from local storage and insert to div creation loop

--- README.md
+++ README.md
@@ -6,8 +6,17 @@ This task required improving some given code for a Work Day Scheduler. Having be
 
 ## Usage
 
+Below is a link to the live site and screenshots of the page showing it deployed:
+
+- 
+
+<img src="./assets/images/Screenshot-1.png" alt="Screenshot 1 with blank input" width=50% height=50% />
+<img src="./assets/images/Screenshot-2.png" alt="Screenshot 2 with saved user input" width=50% height=50% />
+<img src="./assets/images/Screenshot-3.png" alt="Screenshot 3 with inputs saved in local storage" width=50% height=50% />
+
 ## Credits
 
 - https://day.js.org/docs/en/display/format
 - https://stackoverflow.com/questions/30607419/return-only-numbers-from-string
 - https://www.w3schools.com/jquery/jquery_dom_get.asp#:~:text=Get%20Content%20%2D%20text()%2C%20html,the%20value%20of%20form%20fields
+- https://www.w3schools.io/file/markdown-faq/
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,4 @@
 var timeBlockEl = $("#time-box");
-var hourEl = $(".row time-block");
-var pastTime = $(".past");
-var presentTime = $(".present");
-var futureTime = $(".future");
-var tasks = $("<textarea>");
-var taskTextEl = $(".col-8 col-md-10 description").text();
 var workingDayTimes  = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 
 //function to display and format current date in existing HTML header id currentDay
@@ -18,23 +12,28 @@ currentTime();
 //function to create a box for each time in the array of hours in a working day
 function generateTimes(){
 
-  timeBlockEl.empty();
-
   for(i = 0; i < workingDayTimes.length; i++){
     var time = workingDayTimes[i];
 
     var hourEl = $(`<div id="hour-${time}" class="row time-block">`);
 
+    //retrieves value (i.e. task string) of current local storage key (i.e. the time hour - if
+    //none string is empty)
+    var savedTask = localStorage.getItem(`hour-${time}`);
+    console.log(savedTask);
+    if (savedTask === null){
+      savedTask = "";
+    }
+
     var timeBoxEl = $(`<div class="col-2 col-md-1 hour text-center py-3">${time}:00</div>`);
-    var textAreaEl = $(`<textarea class="col-8 col-md-10 description" rows="3"> </textarea>`);
+    var textAreaEl = $(`<textarea class="col-8 col-md-10 description" rows="3">${savedTask}</textarea>`);
     var saveButtonEl = $(`<button class="btn saveBtn col-2 col-md-1" aria-label="save">`);
     var saveStyle = $(`<i class="fas fa-save" aria-hidden="true">`);
-
+    
     saveButtonEl.append(saveStyle);
 
+    //checks current dayjs time against time of the current index of array, adds corresponding class to effect styling
     var currentHour = dayjs().format('H');
-    console.log(currentHour);
-    
     
       if (time < currentHour){
         hourEl.addClass('past');
@@ -44,12 +43,14 @@ function generateTimes(){
          hourEl.addClass('present');
        }
     
-
     hourEl.append(timeBoxEl, textAreaEl, saveButtonEl);
 
     timeBlockEl.append(hourEl);
 
   }
+
+//when save button is clicked, task is saved to local storage, with key corresponding to id of it's 
+// parent attribute, value is content of sibling element. An alert notifies the user their entry is saved
 
   $(".saveBtn").on("click", function(){
     alert("Task saved");
@@ -57,48 +58,13 @@ function generateTimes(){
     var textAreaValue = $thisButton.siblings(".description").val();
     var hourId = $thisButton.parent(".time-block").attr("id");
 
-    console.log(textAreaValue)
     saveTaskToStorage(textAreaValue, hourId)
   });
   
 }
 
-
-
 generateTimes();
-
-$(function () {
-  
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-  });
   
 function saveTaskToStorage(task, id){
   localStorage.setItem(id, task);
 }
-
-function getTaskFromStorage(){
-  var tasks = localStorage.getItem("tasks")
-  if(tasks){
-    tasks = JSON.parse(tasks)
-  } else {
-    tasks = [];
-  }
-  return tasks;
-}
-
